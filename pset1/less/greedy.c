3f82d9b095d429a8ad9b9ee6be518e01bcafafcc
Rob Phoenix <rob@robphoenix.com>
2017-06-16 15:16:31 +0100

    add less/greedy.c

---
+++ pset1/less/greedy.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main() {
+  float change = get_float("How much change is owed?\n");
+  change *= 100;
+  int coins = 0;
+  int cur = 0;
+  float denominations[4] = {25, 20, 5, 1};
+  while (change) {
+    float denomination = denominations[cur];
+    while (change >= denomination) {
+      change -= denomination;
+      coins++;
+    }
+    cur++;
+  }
+  printf("coins: %d\n", coins);
+}
