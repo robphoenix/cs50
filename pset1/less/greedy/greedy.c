9008c63168761c7c59f779670a4f0de99ce8f271
Rob Phoenix <rob@robphoenix.com>
2017-10-10 12:30:01 +0100

    Fix final tests

--- pset1/less/greedy/greedy.c
+++ pset1/less/greedy/greedy.c
@@ -1,14 +1,20 @@
 #include <cs50.h>
+#include <math.h>
 #include <stdio.h>
 
 int main() {
-  float change = get_float("How much change is owed?\n");
-  change *= 100;
+  float owed = -1;
+  while (owed < 0) {
+    owed = get_float("How much change is owed?\n");
+  }
+  owed *= 100;
+  int change;
+  change = (int)round(owed);
   int coins = 0;
   int cur = 0;
-  float denominations[4] = {25, 10, 5, 1};
+  int denominations[4] = {25, 10, 5, 1};
   while (change) {
-    float denomination = denominations[cur];
+    int denomination = denominations[cur];
     while (change >= denomination) {
       change -= denomination;
       coins++;
